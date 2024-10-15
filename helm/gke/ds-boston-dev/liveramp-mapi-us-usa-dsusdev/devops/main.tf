c81e86637c0caa804db2504cc0a7a78624b290c1
chiyingqianhuaner <ccai@liveramp.com>
2024-10-15 09:31:09 +0800

    test

--- helm/gke/ds-boston-dev/liveramp-mapi-us-usa-dsusdev/devops/main.tf
+++ helm/gke/ds-boston-dev/liveramp-mapi-us-usa-dsusdev/devops/main.tf
@@ -11,3 +11,17 @@ module "helm_init" {
 module "cert" {
   source = "git@github.com:LiveRamp/infrastructure.git//_modules/cert/dev"
 }
+
+module "honeypot_template" {
+  source  = "terraform-google-modules/vm/google//modules/instance_template"
+  version = "7.3.0"
+
+  region     = "us-central1"
+  project_id = local.env.project
+
+  subnetwork         = "eng-ops-canary-honeypot-dev-usc1"
+
+  source_image_family  = "thinkst-canary"
+  source_image_project = "thinkst-canary-5a6d1666f17f4e7"
+
+}
