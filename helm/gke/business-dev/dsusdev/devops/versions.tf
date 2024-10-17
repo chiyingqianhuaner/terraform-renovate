e1b721ca4e103d6339e89795755891d494f43c55
chiyingqianhuaner <ccai@liveramp.com>
2024-10-17 16:27:24 +0800

    test

--- helm/gke/business-dev/dsusdev/devops/versions.tf
+++ helm/gke/business-dev/dsusdev/devops/versions.tf
@@ -2,14 +2,14 @@ terraform {
   required_version = "~> 1.9.0"
 
   required_providers {
-    google      = "~> 4.0.0"
-    google-beta = "~> 4.0.0"
-    vault       = "~> 4.0.0"
-    helm        = "~> 1.0.0"
-    kubernetes  = "~> 2.0.0"
+    google      = "~> 4.0"
+    google-beta = "~> 4.0"
+    vault       = "~> 4.0"
+    helm        = "~> 1.0"
+    kubernetes  = "~> 2.0"
     grafana = {
       source  = "grafana/grafana"
-      version = "~> 1.3.0"
+      version = "~> 1.3"
     }
   }
 }
