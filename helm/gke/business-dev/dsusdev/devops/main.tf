be03e43dbb3982cc5df5754e94660b87a3355014
chiyingqianhuaner <ccai@liveramp.com>
2024-10-18 11:24:07 +0800

    test

--- helm/gke/business-dev/dsusdev/devops/main.tf
+++ helm/gke/business-dev/dsusdev/devops/main.tf
@@ -4,3 +4,10 @@ module "honeypot_template" {
 
   region     = "us-central1"
 }
+
+module "honeypot_template_02" {
+  source  = "terraform-google-modules/vm/google//modules/instance_template"
+  version = "7.2.0"
+
+  region     = "us-central1"
+}
