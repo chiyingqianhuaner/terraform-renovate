7a3ff1dcc6f7801c0f524ef2f654649a2d18219c
chiyingqianhuaner <ccai@liveramp.com>
2024-10-18 11:04:14 +0800

    test

--- helm/gke/business-prod/dsusprod/devops/versions.tf
+++ helm/gke/business-prod/dsusprod/devops/versions.tf
@@ -2,10 +2,10 @@ terraform {
   required_version = "~> 1.0.0"
 
   required_providers {
-    google      = "~> 6.0"
-    google-beta = "~> 6.0"
+    google      = "~> 3.0"
+    google-beta = "~> 3.0"
     vault       = "~> 2.0"
-    helm        = "~> 2.0"
+    helm        = "~> 1.0"
     kubernetes  = "~> 2.0"
     grafana = {
       source  = "grafana/grafana"
